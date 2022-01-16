aafc88a07a53be203becaded0c680c538d0ada53
fyk7 <tfscofield115@gmail.com>
2022-01-16 22:10:35 +0900

    separate k8s env by kustomize (local, stg, prd)

--- kustomize_commands.sh
+++ kustomize_commands.sh
@@ -1,2 +1,5 @@
-kustomize build k8s/base | kubectl apply -f -
-kustomize build k8s/base | kubectl delete -f -
+kustomize build k8s/local | kubectl apply -f -
+kustomize build k8s/local | kubectl delete -f -
+
+kustomize build k8s/overlays/stg | kubectl apply -f -
+kustomize build k8s/overlays/stg | kubectl delete -f -
