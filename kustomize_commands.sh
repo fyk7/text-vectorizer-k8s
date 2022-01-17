b0d54395c9e414a6f003081ef64aff4c3908916a
fyk7 <tfscofield115@gmail.com>
2022-01-18 01:10:07 +0900

    update kustomize command

--- kustomize_commands.sh
+++ kustomize_commands.sh
@@ -1,5 +1,8 @@
-kustomize build k8s/local | kubectl apply -f -
-kustomize build k8s/local | kubectl delete -f -
+kustomize build k8s/overlays/local | kubectl apply -f -
+kustomize build k8s/overlays/local | kubectl delete -f -
 
 kustomize build k8s/overlays/stg | kubectl apply -f -
 kustomize build k8s/overlays/stg | kubectl delete -f -
+
+kustomize build k8s/overlays/prd | kubectl apply -f -
+kustomize build k8s/overlays/prd | kubectl delete -f -
