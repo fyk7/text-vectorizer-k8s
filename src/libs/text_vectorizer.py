96f9cfa5d16d32ba6729631bda90357b820af992
fyk7 <tfscofield115@gmail.com>
2022-07-09 23:08:17 +0900

    change comments from jp to en

--- src/libs/text_vectorizer.py
+++ src/libs/text_vectorizer.py
@@ -11,7 +11,6 @@ MAX_LENGTH = 256
 DEVICE = "gpu" if torch.cuda.is_available() else "cpu"
 
 
-# TODO calc_similarity部分はtokenizer, modelに依存しないため、別のクラス・関数に切り出す
 class TextVectorizer(object):
     tokenizer = BertJapaneseTokenizer.from_pretrained(MODEL_NAME)
     model = BertModel.from_pretrained(MODEL_NAME).to(DEVICE)
@@ -19,16 +18,16 @@ class TextVectorizer(object):
     @staticmethod
     def vectorize(text: str) -> np.ndarray:
         """
-        文章をベクトル化する関数
+        Method for vectorizing text.
         Parameters
         ----------
         text : str
-             ベクトル化する文章
+             Text to be vectorized.
         """
         if not isinstance(text, str):
-            raise TypeError(f"引数はstring型にしてください!")
+            raise TypeError(f"Args should be str type.")
         if len(text) >= MAX_LENGTH:
-            raise ValueError(f"文字数は{MAX_LENGTH}文字以下にしてください!")
+            raise ValueError(f"Text length should be below {MAX_LENGTH}")
         encoding = TextVectorizer.tokenizer(
             text, 
             max_length=MAX_LENGTH, 
@@ -55,13 +54,13 @@ class TextVectorizer(object):
         eps: float = 1e-9,
     ) -> float:
         """
-        二つのベクトル化された文章の類似度を算出する関数
+        Function to calculate the similarity between two vectorized sentences
         Parameters
         ----------
         sent_vector1, sent_vector2 : t.Union[np.array, pd.Series, t.List[float]]
-            文章をベクトル化したもので、構成要素がfloatな一次元のベクトル
+            Vectorized text.
         eps : float, default=1e-9
-            ベクトルを正規化する際のノルムが0の場合ZeroDevisionErrorを回避するための小さな数
+            A small number to avoid ZeroDevisionError if the norm is 0 when normalizing a vector.
         """
         
         def __convert_vec2numpy(vect: t.Any) -> np.ndarray:
@@ -72,7 +71,7 @@ class TextVectorizer(object):
             elif isinstance(vect, list):
                 return np.array(vect)
             else:
-                raise TypeError("引数は、np.array, pd.Series, List[float]のみ許容されています!")
+                raise TypeError("Args should be in (np.array, pd.Series, List[float])")
 
         sent_vector1 = __convert_vec2numpy(sent_vector1)
         sent_vector2 = __convert_vec2numpy(sent_vector2)
@@ -90,16 +89,13 @@ class TextVectorizer(object):
         eps: float = 1e-9,
     ) -> np.ndarray:
         """
-        最も類似している文章のindexの配列を返す
-        例) 戻り値が[1, 0, 0]の場合、
-        index=0の文章はindex=1の文章と最も類似していることとなる
-        index=1の文章はindex=0の文章と最も類似していることとなる
+        Returns an array of the indexes of the most similar texts.
         Parameters
         ----------
         sent_vectors : t.Union[np.array, pd.DataFrame, t.List[float]]
-            文章をベクトル化したもののリストで、構成要素がfloatな二次元のベクトル
+            Vectorized text.
         eps : float, default=1e-9
-            ベクトルを正規化する際のノルムが0の場合ZeroDevisionErrorを回避するための小さな数
+            A small number to avoid ZeroDevisionError if the norm is 0 when normalizing a vector.
         """
 
         def __convert_vec2numpy_2dim(vect: t.Any) -> np.ndarray:
@@ -110,9 +106,9 @@ class TextVectorizer(object):
             elif isinstance(vect, list):
                 tmp_vect = np.array(vect)
             else:
-                raise TypeError("引数の型は、np.array, pd.Series, List[float]のみ許容されています!")
+                raise TypeError("Args should be in (np.array, pd.Series, List[float])")
             if tmp_vect.ndim != 2:
-                raise ValueError("calc_similarity_multiではベクトルの次元は二次元にしてください!")
+                raise ValueError("The dimension of the vector should be two-dimensional.")
             return tmp_vect
 
         sent_vectors = __convert_vec2numpy_2dim(sent_vectors)
@@ -121,16 +117,15 @@ class TextVectorizer(object):
         sent_vectors_normalized = sent_vectors / (norm + eps)
         sim_matrix = sent_vectors_normalized.dot(sent_vectors_normalized.T)
 
-        # 入力と同じ記事が出力されることを避けるため、
-        # 類似度行列の対角要素の値を小さくしておく。
+        # To avoid outputting the same articles as the input.
         np.fill_diagonal(sim_matrix, -1)
         similars = sim_matrix.argmax(axis=1)
         return similars
 
 
 if __name__ == "__main__":
-    tennis_vector = TextVectorizer.vectorize("週末はテニスに行こう")
-    soccer_vector = TextVectorizer.vectorize("サッカーの試合を見よう")
-    dentist_vector = TextVectorizer.vectorize("虫歯が痛い")
+    tennis_vector = TextVectorizer.vectorize("週末は映画を見に行こう")
+    soccer_vector = TextVectorizer.vectorize("野球の試合を見よう")
+    dentist_vector = TextVectorizer.vectorize("歯医者に行かなければ")
     print(TextVectorizer.calc_similarity(tennis_vector, soccer_vector))
     print(TextVectorizer.calc_similarity(tennis_vector, dentist_vector))
